bf55668e56db1d83c3e47971097b2724f3e699c3
Piyush Rajput <piyushrajput1519@gmail.com>
2020-12-17 11:46:12 +0530

    Add Utils

---
+++ src/utils/InjectClassName/InjectClassName.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import InjectClassName from './InjectClassName';
+
+describe('InjectClassName', () => {
+    it('should inject className prop to all children', () => {
+        const { container } = render(
+            <InjectClassName className="test-className">
+                <div>Test div 1</div>
+                <div>Test div 2</div>
+            </InjectClassName>
+        );
+        expect(container).toMatchSnapshot();
+    });
+});
---
+++ src/utils/Styled/WithThemeProp.ts
@@ -0,0 +1,5 @@
+import React from "react"
+export interface WithThemeProp {
+    // eslint-disable-next-line no-undef
+    as?: keyof JSX.IntrinsicElements | React.ComponentType<any>;
+}
